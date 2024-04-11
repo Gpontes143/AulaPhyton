c42305dd781536f89ce8758a2a53c9e4529fc99c
Gpontes143 <Gpontes143@gmail.com>
2024-04-11 09:06:46 -0300

    mudei e melhorei

---
+++ Java/CalculadoraRefrigerante.java
@@ -0,0 +1,33 @@
+public class CalculadoraRefrigerante {
+
+    public static void main(String[] args) {
+        // Definindo o preço por unidade de refrigerante
+        double precoPorUnidade = 4.99;
+
+        // Definindo o preço por unidade com desconto para compras a partir de 6 unidades
+        double precoPorUnidadeComDesconto = 4.19;
+
+        // Inicializando o scanner para ler a entrada do usuário
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        // Solicitando a quantidade desejada de refrigerantes
+        System.out.println("Quantos refrigerantes de 600ml você deseja comprar?");
+        int quantidade = scanner.nextInt();
+
+        // Calculando o valor total com base na quantidade adquirida
+        double valorTotal;
+
+        if (quantidade < 6) {
+            valorTotal = precoPorUnidade * quantidade;
+        } else {
+            valorTotal = precoPorUnidadeComDesconto * quantidade;
+        }
+
+        // Exibindo o resultado
+        System.out.println("Quantidade de refrigerantes: " + quantidade);
+        System.out.println("Valor total a ser pago: R$ " + valorTotal);
+
+        // Fechando o scanner
+        scanner.close();
+    }
+}
--- Java/exercicio-caso1.java
+++ Java/exercicio-caso1.java
@@ -1,68 +1,57 @@
-//Já tive alguma expêriencia em java e codigos
+class EntradaDoCinema {
 
-class Entrada_do_Cinema {
-
-    //
     public static void main(String[] args) {
+        // Guilherme Pontes Campos
 
-        //criando objeto para analisar a escolha
         java.util.Scanner leitor = new java.util.Scanner(System.in);   
 
-        //valor da entrada inteira
         double valorInteira = 32.00;
 
-        //fazendo repetições para caso o usuario escolha o incorreto, ele possa retornar as opções
         while (true) {
-        //Fazendo as escolhas para o usuario
-        System.out.println("Selecione uma opção:");
-        System.out.println("1. Entrada inteira");
-        System.out.println("2. Meia entrada para crianças até 12 anos");
-        System.out.println("3. Meia entrada para estudantes");
+            System.out.println("Selecione uma opção:");
+            System.out.println("1. Entrada inteira");
+            System.out.println("2. Meia entrada para crianças até 12 anos");
+            System.out.println("3. Meia entrada para estudantes");
 
-        int opcao = leitor.nextInt();
+            int opcao = leitor.nextInt();
 
-        double preço = 0.0;
+            double preco = 0.0;
 
-        switch (opcao) {
-            case 1:
-            //se escolher a opção 1, o preço é o valor da entrada inteira = 32.00$
-            preço = valorInteira;
-            System.out.println("Entrada de $" + valorInteira );
-                break;
-            case 2:
-            //se escolher a opção 2, o preço é o valor da entrada inteira = 32.00$/2 = 16.0
-                System.out.println("Informe a idade da criança:");
-                int idade = leitor.nextInt();
-                if (idade <= 12) {
-                    preço = valorInteira / 2; // Meia entrada
-                    System.out.println("Entrada de $" + valorInteira );
-                    break;
-                } else {
-                    System.out.println("Essa criança não se qualifica para meia entrada.");
-                    preço = valorInteira; 
+            switch (opcao) {
+                case 1:
+                    preco = valorInteira;
+                    System.out.println("Valor pago: $" + preco);
+                    return;
+                case 2:
+                    System.out.println("Informe a idade da criança:");
+                    int idade = leitor.nextInt();
+                    if (idade <= 12) {
+                        preco = valorInteira / 2;
+                        System.out.println("Valor pago: $" + preco);
+                        return;
+                    } else {
+                        System.out.println("Essa criança não se qualifica para meia entrada, por favor selecione outra opção.");
+                        continue;
+                    }
+                case 3:
+                    System.out.println("Você possui carteira de estudante? (S/N)");
+                    String resposta = leitor.next();
+                    if (resposta.equalsIgnoreCase("S")) {
+                        preco = valorInteira / 2;
+                        System.out.println("Valor pago: $" + preco);
+                        return;
+                    } else {
+                        System.out.println("Você não possui carteira de estudante, por favor selecione outra opção.");
+                        continue;
+                    }   
+                default:
+                    System.out.println("Opção inválida. Por favor, selecione uma opção válida.");
                     continue;
-                }
-                
-            case 3:
-                System.out.println("Você possui carteira de estudante? (S/N)");
-                String resposta = leitor.next();
-                if (resposta.equalsIgnoreCase("S")) {
-                    preço = valorInteira / 2; // Meia entrada
-                    break;
-                } else {
-                    System.out.println("Você não possui carteira de estudante, por favor selecione outra opção.");
-                    preço = valorInteira; // Entrada inteira
-                    continue;
-                }   
-                
-            default:
-                System.out.println("Opção inválida. Por favor, selecione uma opção válida.");
-                continue;
-            
-    
-
+                    
             }
-
+            
         }
+            
     }
+   
 }
