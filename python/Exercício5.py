74de41d52e785888ca08e80c9fdf1259ada056fe
Gpontes143 <Gpontes143@gmail.com>
2024-04-12 09:20:53 -0300

    c

--- python/Exercício5.py
+++ python/Exercício5.py
@@ -1,12 +1,25 @@
 g = int(input("escolha um número"))   
-
+""""
 lista = []
-for i in range(1, 21):
+for i in range(1, 11):
     lista.append(g*i)
 
 print("a tabuada do seu número é:",f'{lista}') 
 
-  
+""" 
+
+n1 = g*1
+n2 = g*2
+n3 = g*3
+n4 = g*4
+n5 = g*5
+n6 = g*6   
+n7 = g*7
+n8 = g*8
+n9 = g*9
+n10 = g*10
+print("a tabuada do seu número é:",f'{n1}, {n2}, {n3}, {n4}, {n5}, {n6}, {n7}, {n8}, {n9}, {n10}')
+
 
 
 
--- python/desafioju1.py
+++ python/desafioju1.py
@@ -1,5 +1,5 @@
 def primo(n):
-    if n <= 1:
+    if n <= 1: 
         return False
     if n <= 3:
         return True
