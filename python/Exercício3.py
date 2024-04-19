e601d48bbc70695e3bb6e8ce1801acb97171170d
Gpontes143 <Gpontes143@gmail.com>
2024-04-19 09:32:49 -0300

    .

--- python/Exercício3.py
+++ python/Exercício3.py
@@ -2,4 +2,4 @@ n = int(input("escolha um número"))
 n1 = n-1 
 n2 = n+1
 
-print("o antecessor e o sucessor são respectivamente: ", f'  {n1} "e"  {n2}')
+print("Analisando o valor escolhido, o antecessor e o sucessor são respectivamente: ", f'{n1} e {n2}')
