4b9b499022dee72a5e042a0bae5a5e94db853083
Gpontes143 <Gpontes143@gmail.com>
2024-04-19 09:37:34 -0300

    a

--- python/Exercício1.py
+++ python/Exercício1.py
@@ -1,6 +1,6 @@
 
 n = int(input("escolha um número"))
 n1 = n*2 
-n2 = n**3
+n2 = n*3
 n3 = n**(1/2)
-print(f'{n1}, {n2}, {n3}')
+print(f'o dobro do valor escolhido é: {n1}, o triplo do valor escolhido é:{n2}, e a raiz quadrada é:{n3}')
